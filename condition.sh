2d7fe7256482fdb1e4856c9643c58a44f8b646c9
NamanKajla <nkajla0906@gmail.com>
2026-08-18 11:54:43 +0530

    conditions

---
+++ condition.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+read -p "Enter your age: " age
+
+if [ $age -le 0 ]; then
+    echo "Invalid age entered. Please enter a valid age."
+elif [ $age -lt 13 ]; then
+    echo "You are a child."
+elif [ $age -lt 18 ]; then
+    echo "You are a teenager."
+else
+    echo "You are an adult."
+fi
